18730a52e5dc3c790ea69824f9ce30bf109bd568
Shayla Choque <U20201C144@upc.edu.pe>
2023-06-26 03:17:27 -0500

    Mealthy Backend Testing
    Testing implemetation

---
+++ MealthyTest/Drivers/Driver.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace MealthyTest.Drivers
+{
+    public class Driver
+    {
+    }
+}
---
+++ MealthyTest/Hooks/Hook.cs
@@ -0,0 +1,10 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace MealthyTest.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+    }
+}
